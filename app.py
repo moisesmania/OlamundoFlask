0393afba5a0cba3ecdfc90b5c80fb97269f40f9b
moisesmania <moisesmotot@gmail.com>
2025-06-16 09:12:07 -0300

    first commitolamoises

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Olá Mundo do Flask no GitHub Actions!"
+    return "Olá Mundo do Moisés!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
